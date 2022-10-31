fea4421834aebc62e5c95340777729d1decdce8f
Harrison Sabean <hsabes@Harrisons-iMac.local>
2022-10-31 01:04:52 -0400

    initial commit, created chess board with tiles

---
+++ scripts/index.js
@@ -0,0 +1,53 @@
+// This function determines which squares are white or black
+
+function determineSquareColor(element, i){
+    if (i >= 0 && i <= 7){
+        i % 2 === 0 ? element.style.backgroundColor = "#FFF" : element.style.backgroundColor = "#000";
+    }
+    if (i >= 8 && i <= 15){
+        i % 2 !== 0 ? element.style.backgroundColor = "#FFF" : element.style.backgroundColor = "#000";
+    }
+    if (i >= 16 && i <= 23){
+        i % 2 === 0 ? element.style.backgroundColor = "#FFF" : element.style.backgroundColor = "#000";
+    }
+    if (i >= 24 && i <= 31){
+        i % 2 !== 0 ? element.style.backgroundColor = "#FFF" : element.style.backgroundColor = "#000";
+    }
+    if (i >= 32 && i <= 39){
+        i % 2 === 0 ? element.style.backgroundColor = "#FFF" : element.style.backgroundColor = "#000";
+    }
+    if (i >= 40 && i <= 47){
+        i % 2 !== 0 ? element.style.backgroundColor = "#FFF" : element.style.backgroundColor = "#000";
+    }
+    if (i >= 48 && i <= 55){
+        i % 2 === 0 ? element.style.backgroundColor = "#FFF" : element.style.backgroundColor = "#000";
+    }
+    if (i >= 56 && i <= 63){
+        i % 2 !== 0 ? element.style.backgroundColor = "#FFF" : element.style.backgroundColor = "#000";
+    }
+}
+
+// This function renders all 64 squares onto the chess board and may handle appending pieces to a particular square based off of it's index
+
+function renderSquares(){
+    numOfSquares = 0;
+    const chessBoard = document.querySelector(".chessBoard")
+    const chessSquareArray = []
+    while (numOfSquares < 64){
+        chessSquare = document.createElement("div")
+        chessSquare.classList.add("chessSquare")
+        chessSquareArray.push(chessSquare)
+        numOfSquares += 1;
+    }
+    chessSquareArray.forEach((element, i) => {
+        determineSquareColor(element, i)
+        element.addEventListener("click", function(){
+            console.log(i)
+        })
+    })
+    return chessSquareArray.forEach((element) => {
+        return chessBoard.append(element)
+    })
+}
+
+renderSquares()
