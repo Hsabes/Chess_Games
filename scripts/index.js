4dce5a8bd11a706298ccabccaa98a002d0a4f1d8
Harrison Sabean <hsabes@Harrisons-iMac.local>
2022-10-31 01:55:48 -0400

    refactored determineSquareColor() function to be more clean

--- scripts/index.js
+++ scripts/index.js
@@ -1,36 +1,18 @@
 // This function determines which squares are white or black
 
 function determineSquareColor(element, i){
-    if (i >= 0 && i <= 7){
-        i % 2 === 0 ? element.style.backgroundColor = "#FFF" : element.style.backgroundColor = "#000";
+    if ((i >= 0 && i <= 7) || (i >= 16 && i <= 23) || (i >= 32 && i <= 39) || (i >= 48 && i <= 55)){
+        return i % 2 === 0 ? element.style.backgroundColor = "#FFF" : element.style.backgroundColor = "#000";
     }
-    if (i >= 8 && i <= 15){
-        i % 2 !== 0 ? element.style.backgroundColor = "#FFF" : element.style.backgroundColor = "#000";
-    }
-    if (i >= 16 && i <= 23){
-        i % 2 === 0 ? element.style.backgroundColor = "#FFF" : element.style.backgroundColor = "#000";
-    }
-    if (i >= 24 && i <= 31){
-        i % 2 !== 0 ? element.style.backgroundColor = "#FFF" : element.style.backgroundColor = "#000";
-    }
-    if (i >= 32 && i <= 39){
-        i % 2 === 0 ? element.style.backgroundColor = "#FFF" : element.style.backgroundColor = "#000";
-    }
-    if (i >= 40 && i <= 47){
-        i % 2 !== 0 ? element.style.backgroundColor = "#FFF" : element.style.backgroundColor = "#000";
-    }
-    if (i >= 48 && i <= 55){
-        i % 2 === 0 ? element.style.backgroundColor = "#FFF" : element.style.backgroundColor = "#000";
-    }
-    if (i >= 56 && i <= 63){
-        i % 2 !== 0 ? element.style.backgroundColor = "#FFF" : element.style.backgroundColor = "#000";
+    if ((i >= 8 && i <= 15) || (i >= 24 && i <= 31) || (i >= 40 && i <= 47) || (i >= 56 && i <= 63)){
+        return i % 2 !== 0 ? element.style.backgroundColor = "#FFF" : element.style.backgroundColor = "#000";
     }
 }
 
 // This function renders all 64 squares onto the chess board and may handle appending pieces to a particular square based off of it's index
 
 function renderSquares(){
-    numOfSquares = 0;
+    let numOfSquares = 0;
     const chessBoard = document.querySelector(".chessBoard")
     const chessSquareArray = []
     while (numOfSquares < 64){
